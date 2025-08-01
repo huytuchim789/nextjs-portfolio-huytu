bde9ca7e6fc143a80fb05532d710be241b6a258a
loki-mfxdev <tran.tu.huy@moneyforward.co.jp>
2025-08-01 15:28:48 +0900

    Add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Photo from "@/components/Photo";
+import Socials from "@/components/Socials";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { FiDownload } from "react-icons/fi";
@@ -5,9 +7,37 @@ export default function Home() {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb2">
-          <div>text</div>
-          <div>photo</div>
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1">
+              Hello I'm
+              <br /> <span className="text-accent">Loki</span>
+            </h1>
+
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and i am
+              proficient in various programing languages and technologies
+            </p>
+
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant={"outline"}
+                size={"lg"}
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          <Photo></Photo>
         </div>
       </div>
     </section>
---
+++ components/Socials.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import path from "path";
+import React, { CSSProperties } from "react";
+
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  {
+    icon: <FaGithub />,
+    path: "",
+  },
+  {
+    icon: <FaLinkedinIn />,
+    path: "",
+  },
+  {
+    icon: <FaYoutube />,
+    path: "",
+  },
+  {
+    icon: <FaTwitter />,
+    path: "",
+  },
+];
+export default function Socials({
+  containerStyles,
+  iconStyles,
+}: {
+  containerStyles: string;
+  iconStyles: string;
+}) {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
